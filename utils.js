59bf99a854228ee093826820af8bbd5ebdc38729
ChitLay118 <c388925@gmail.com>
2025-11-27 17:03:34 +0700

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-export function timeAgo(d){
-  if(!d) return '';
-  const s = Math.floor((Date.now() - d.getTime())/1000);
-  if(s<60) return `${s}s`;
-  if(s<3600) return `${Math.floor(s/60)}m`;
-  if(s<86400) return `${Math.floor(s/3600)}h`;
-  return `${Math.floor(s/86400)}d`;
-}
